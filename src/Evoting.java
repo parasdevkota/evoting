35f530284f96b8a2a0e84171c2cf53827835b8e4
Parash Devkota <parasdevkota@gmail.com>
2017-08-20 09:06:31 +0545

    20 aug, database lib + src
    JDBC driver for database has been added.
    Source code minor change (connecting db)

---
+++ src/Evoting.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+class Evoting{
+	static final String JDBC_DRIVER
+	static final String DB_URL
+	static Connection conn;
+	static Statement stmt;
+
+	public static void main(String args[]){
+		try{
+			Class.forName();
+			conn = DriverManager.openConnection();
+			
+			stmt
+			String sql
+			int r_affected = stmt.updateQuery(sql);
+		}
+	}
+
+	public static void add_candidate(String name){
+		try{
+			stmt.executeQuery("");
+		}
+		catch(SQLException se){
+			System.out.println("Error. Can't add candidates.");
+		}
+
+	}
+
+}
