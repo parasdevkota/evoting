c16e06daf571c5399544f96f01794faf97393c4d
Parash Devkota <parasdevkota@gmail.com>
2017-08-28 11:13:33 +0545

    Adds Log in

--- src/Evoting.java
+++ src/Evoting.java
@@ -2,26 +2,42 @@ package evoting;
 import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Evoting{
-	
-
-
+class Evoting{
+	static Connection conn = null;
+	static Statement stmt = null;
+	static JFrame window;
 	public static void main(String args[]){
-	JFrame window = new JFrame("Evoting");
-	window.setVisible(true);
-	window.setSize(400,400);
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	LoginDialog ld = new LoginDialog(window);
+		JLabel user_label;
+	  	JTextField user;
+	  	JLabel pass_label;
+	  	JPasswordField pass;
+	  	JButton btn;
+	  	ResultSet rs;
+		final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+		final String DB_URL = "jdbc:mysql://localhost:3306/?useSSL=false";
+		final String USER = "root";
+		final String PASS = "";
+		
 
+		user_label = new JLabel("Username:");
+	    pass_label = new JLabel("Password:");
+	    user = new JTextField(25);
+	    pass = new JPasswordField(25);
+	    btn = new JButton("Login");
+		window = new JFrame("Evoting");
+		window.setVisible(true);
+		window.setLayout(new FlowLayout());
+		window.setSize(400,250);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(user_label);
+	    window.add(user);
+	    window.add(pass_label);
+	    window.add(pass);
+	    window.add(btn);
 
-	final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	final String DB_URL = "jdbc:mysql://localhost:3306/?useSSL=false";
-	final String USER = "root";
-	final String PASS = "";
-	Connection conn = null;
-	Statement stmt = null;
-		try{
+	    try{
 			System.out.println("Connecting to database server...");
 
 			Class.forName("com.mysql.jdbc.Driver");
@@ -35,12 +51,34 @@ public class Evoting{
 			stmt.execute("USE evoting_db");
 			stmt.execute("CREATE TABLE IF NOT EXISTS voter(id smallint AUTO_INCREMENT, dob date, name varchar(40), address varchar(50), securityKey char(10), hasVoted boolean, PRIMARY KEY (id))");
 			stmt.execute("CREATE TABLE IF NOT EXISTS candidate(id smallint AUTO_INCREMENT, name varchar(40), noOfVotes smallint, symbolPath varchar(50), PRIMARY KEY (id))");
-			// stmt.execute("CREATE TABLE IF NOT EXISTS borrows(id smallint(6) AUTO_INCREMENT, borrower_id smallint(6), book_id smallint(6), borrowed_date timestamp, PRIMARY KEY (id))");
-			// stmt.execute("CREATE TABLE IF NOT EXISTS settings(id smallint(6) AUTO_INCREMENT, password varchar(20), threshold tinyint(4), overdue_duration tinyint(4), PRIMARY KEY (id))");
+			stmt.execute("CREATE TABLE IF NOT EXISTS admin(id smallint AUTO_INCREMENT, username varchar(40), password varchar(20), PRIMARY KEY (id))");
 		}catch(Exception e){
 			System.out.println("ERROR: Error while loading from database.");
 			e.printStackTrace();
-
 		}
+	    btn.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent ae){
+	    		try{
+
+	    			ResultSet rs = stmt.executeQuery("SELECT * from admin");
+	    			boolean sucess = false;
+		    		while(rs.next()){
+		    			if(rs.getString("username").equals(user.getText()) && rs.getString("password").equals(pass.getText())){
+		    				JOptionPane.showMessageDialog(window, "Login Sucessful", "Success", JOptionPane.INFORMATION_MESSAGE);
+		    				sucess = true;
+
+		    			}
+		    		}
+		    		if(!sucess){
+		    			JOptionPane.showMessageDialog(window, "Username and password do not match", "Login Failed", JOptionPane.ERROR_MESSAGE);
+
+		    		}
+	    		}
+	    		catch(Exception e){
+
+	    		}
+	    	}
+	    });
 	}
+	
 }
